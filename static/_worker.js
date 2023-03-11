53def6b8649e8d3baf578535c186596e93eace51
koura718 <koura718@gmail.com>
2023-03-11 15:25:51 +0900

    20230311-1438a

--- static/_worker.js
+++ static/_worker.js
@@ -1,5 +1,17 @@
 export default {
-  fetch(request, env, context) {
-    return new Response('Hello');
-  },
-};
+    async fetch(request, env, context) {
+      // Forward / Proxy original request
+      let res = await fetch(request);
+  
+      // Add custom header(s)
+      res = new Response(res.body, res);
+      res.headers.set('x-foo', 'bar');
+  
+      // Cache the response
+      // NOTE: Does NOT block / wait
+      context.waitUntil(caches.default.put(request, res.clone()));
+  
+      // Done
+      return res;
+    },
+  };
