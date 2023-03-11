ceda7d04cbc8763f7b7ef6e838161f7b51a531da
koura718 <koura718@gmail.com>
2023-03-11 15:11:48 +0900

    20230311-1438a

--- static/_worker.js
+++ static/_worker.js
@@ -1,18 +1,8 @@
 // Format: Module Worker
 export default {
     async fetch(request, env, context) {
-      // Forward / Proxy original request
-      let res = await fetch(request);
-  
-      // Add custom header(s)
-      // res = new Response(res.body, res);
-      // res.headers.set('x-foo', 'bar');
-  
-      // Cache the response
-      // NOTE: Does NOT block / wait
-      // context.waitUntil(caches.default.put(request, res.clone()));
-  
-      // Done
-      return res;
+      // Proxy to origin on unhandled/uncaught exceptions
+      context.passThroughOnException();
+      throw new Error('Oops');
     },
   };
