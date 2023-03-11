63bfd362bef1f8af1e0cbe381cdc4fc1b3817100
koura718 <koura718@gmail.com>
2023-03-11 14:55:37 +0900

    20230311-1438a

--- static/_worker.js
+++ static/_worker.js
@@ -1,5 +1,15 @@
-export default {
-  fetch(request, env, context) {
-    return new Response('Hello');
-  },
-};
+// export default {
+//   fetch(request, env, context) {
+//     return new Response('Hello');
+//   },
+// };
+
+addEventListener('fetch', event => {
+    let { pathname } = new URL(event.request.url);
+  
+    // Allow "/ignore/*" URLs to hit origin
+    if (pathname.startsWith('/ignore/')) return;
+  
+    // Otherwise, respond with something
+    event.respondWith(handler(event));
+  });
